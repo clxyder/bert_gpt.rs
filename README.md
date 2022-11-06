4e682a34a7b53a2ee8871d043d7b63deef88e520
Carlos A. Wong <carloswong54@gmail.com>
2022-11-05 22:09:20 -0400

    working example with 125M parameter model

---
+++ README.md
@@ -0,0 +1,7 @@
+# Running GPT Neo with Rust
+
+The project was originally inspired from [here](https://github.com/Me163/youtube/tree/main/bert_test).
+
+## Resources
+
+* [Gpt-Neo Low VRAM](https://github.com/arrmansa/Basic-UI-for-GPT-Neo-with-low-vram)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use rust_bert::{pipelines::{text_generation::{TextGenerationConfig, TextGenerationModel}, common::ModelType}};
+use rust_bert::resources::RemoteResource;
+use rust_bert::gpt_neo::GptNeoModelResources;
+use rust_bert::gpt_neo::GptNeoMergesResources;
+use rust_bert::gpt_neo::GptNeoVocabResources;
+use rust_bert::gpt_neo::GptNeoConfigResources;
+
+use stopwatch::Stopwatch;
+
 fn main() {
     println!("Hello, world!");
+    let model_resource = Box::new(RemoteResource::from_pretrained(
+        GptNeoModelResources::GPT_NEO_125M,
+    ));
+    let config_resource = Box::new(RemoteResource::from_pretrained(
+        GptNeoConfigResources::GPT_NEO_125M,
+    ));
+    let vocab_resource = Box::new(RemoteResource::from_pretrained(
+        GptNeoVocabResources::GPT_NEO_125M,
+    ));
+    let merges_resource = Box::new(RemoteResource::from_pretrained(
+        GptNeoMergesResources::GPT_NEO_125M,
+    ));
+
+    let generate_config = TextGenerationConfig {
+        model_type: ModelType::GPTNeo,
+        model_resource,
+        config_resource,
+        vocab_resource,
+        merges_resource,
+        num_beams: 5,
+        no_repeat_ngram_size: 2,
+        max_length: 200,
+        // device: tch::Device::Cpu,
+        ..Default::default()
+    };
+
+    let model = TextGenerationModel::new(generate_config).unwrap();
+    println!("Model successfully loaded.");
+
+    loop {
+        println!("Please enter `<prefix>/<text>`: ");
+
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+
+        let split = line.split("/").collect::<Vec<&str>>();
+        let slice = split.as_slice();
+
+        let prefix = slice[0];
+        let text = &slice[1..];
+
+        let sw = Stopwatch::start_new();
+        let output = model.generate(text, Some(prefix));
+        println!("Model output: ");
+        for sentence in output {
+            println!("{}", sentence);
+        }
+        println!("Generating the output from the model took {}ms", sw.elapsed_ms());
+    }
 }
